79b4a0884dfe84da35eeb19b277e8870d5bc83da
Ethanbst <ethan.basti1@gmail.com>
2023-10-09 16:06:54 +0200

    Test page d'accueil

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
-btn1.onclick = test;
-
-
+btn1.onclick = (test);
+btn2.onclick = () => document.body.style.backgroundColor="Red";
+function test(){
+    document.body.style.backgroundColor="Blue";
+}
