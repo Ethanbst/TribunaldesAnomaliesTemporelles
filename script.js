5bb7e372e4e1b36141086dbd1eec5472cf2f84b6
Ethanbst <ethan.basti1@gmail.com>
2023-10-16 16:53:53 +0200

    Ajout d'une fonction animation bloc_titre

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ blocs.forEach(bloc => {
         bloc.classList.remove('bloc-hover');
     });
 });
+
+// Sélectionnez tous les éléments avec la classe "bloc_titre"
+const blocs2 = document.querySelectorAll('.bloc_titre');
+
+// Parcourez chaque élément et ajoutez des gestionnaires d'événements pour "hover"
+blocs2.forEach(bloc_titre => {
+    bloc_titre.addEventListener('mouseenter', () => {
+        bloc_titre.classList.add('bloc-hover');
+    });
+
+    bloc_titre.addEventListener('mouseleave', () => {
+        bloc_titre.classList.remove('bloc-hover');
+    });
+});
