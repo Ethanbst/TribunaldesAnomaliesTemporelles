b0e5da4c1cff579c2cd0ba3afb4aa52dd2663d80
Ethanbst <127886349+Ethanbst@users.noreply.github.com>
2023-11-24 15:54:42 +0100

    Revert "Ethan (#13)"
    This reverts commit fc122b3542a88208e886e94f59bc3517e9e86ab8.

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ blocs.forEach(bloc => {
 
 // Sélectionnez tous les éléments avec la classe "bloc_titre"
 const blocs2 = document.querySelectorAll('.bloc_titre');
+
 // Parcourez chaque élément et ajoutez des gestionnaires d'événements pour "hover"
 blocs2.forEach(bloc_titre => {
     bloc_titre.addEventListener('mouseenter', () => {
@@ -28,8 +29,10 @@ blocs2.forEach(bloc_titre => {
 });
 
 
-// Animation du bouton login
+
 const bt1 = document.querySelectorAll('.bouton_lien');
+
+// Animation du buton login
 bt1.forEach(bouton_lien => {
     bouton_lien.addEventListener('mouseenter', () => {
         bouton_lien.classList.add('bouton_lien_hover');
@@ -40,36 +43,28 @@ bt1.forEach(bouton_lien => {
     });
 });
 
-
-// Animation du bouton de lancement
+// Animation du buton de lancement
 const bt2 = document.querySelectorAll('.bouton_lancement_lien');
+
 bt2.forEach(bouton_lancement_lien => {
     bouton_lancement_lien.addEventListener('mouseenter', () => {
         bouton_lancement_lien.classList.add('bouton_lancement_lien_hover');
     });
+
     bouton_lancement_lien.addEventListener('mouseleave', () => {
         bouton_lancement_lien.classList.remove('bouton_lancement_lien_hover');
     });
 });
 
 
-//Si touche entrer pressée on active active la fonction de validation des id
-document.addEventListener("keypress", function(e) {
+document.addEventListener("keypress", function(e) { //Si touche entrer pressée on active active la fonction de validation des id
     if (e.key === "Enter") {
-        if(window.location.href.includes("login.html") == true){
-            console.log("Page Login");
-            validateLogin();
-        }
-        else if(window.location.href.includes("admin.html") == true){
-            console.log("Page Admin");
-            validateStop();
-        }
+        validateLogin();
     }
 });
 
 
-//Action du bouton login
-async function validateLogin() {
+async function validateLogin() { //Action du bouton login
     console.log("Fonction validateLogin appelée");
     var username = document.getElementById("username").value; //On récupère l'id dans username
     var password = document.getElementById("password").value; //On récupère le mot de passe dans password
@@ -95,30 +90,25 @@ async function validateLogin() {
     if (validCredentials.hasOwnProperty(username_hash) && validCredentials[username_hash] === password_hash) {
         // Authentification réussie, redirigez l'utilisateur vers la page souhaitée
         console.log("Redirection effectuée");
-
-        window.location.href = "admin.html";
-
+        //window.location.href = "index.html";
     } else {
         // Affichez un message d'erreur
         document.getElementById("errorText").textContent = "Identifiant ou mot de passe incorrect.";
     }
 }
 
+    function aleatoire(){ //Génère un chiffre aléatoire entre 100 et 500
+        var alea;
+        alea = Math.floor(Math.random() * (1500 + 1));;
+        console.log(alea);
+        return alea; 
+    }
 
-//Génère un chiffre aléatoire entre 100 et 500
-function aleatoire(){ 
-    var alea;
-    alea = Math.floor(Math.random() * (1500 + 1));;
-    console.log(alea);
-    return alea; 
-}
-
-
-// Attendez 5 secondes avant de masquer l'écran de chargement
-setTimeout(function() {
-    var loader = document.getElementById('loader');
-    loader.style.display = 'none';
-},/*aleatoire()*/); // temps de chargement en millisecondes aléatoire
+        // Attendez 5 secondes avant de masquer l'écran de chargement
+    setTimeout(function() {
+        var loader = document.getElementById('loader');
+        loader.style.display = 'none';
+    },/*aleatoire()*/); // temps de chargement en millisecondes aléatoire
 
 
 // Fonction pour hacher une chaîne
@@ -136,160 +126,3 @@ async function hashString(inputString) {
     //console.log(inputString + "=" + hashedValue);
     return hashedValue;
   }
-
-
-
-//SCRIPT PAGE ADMIN
-
-    // Appelez la fonction lorsque la page est chargée
-    window.onload = mettreAJourRAM();
-    window.onload= mettreAJourGPU('.GPU');
-
-//Génère une valeur aléatoire entre 600 et 1020 et la remplace dans .RAM
-function mettreAJourRAM() {
-  // Sélectionne l'élément div avec la classe "RAM"
-  var divRAM = document.querySelector('.RAM');
-
-  // Met à jour le texte de l'élément toutes les 1 seconde
-  setInterval(function() {
-    // Génère un nombre aléatoire entre 600 et 630
-    var valeurAleatoire = Math.floor(Math.random() * 420) + 600;
-
-    // Remplace le texte de l'élément div par la valeur aléatoire
-    divRAM.textContent = valeurAleatoire;
-  }, 1000); // 1000 millisecondes équivalent à 1 seconde
-}
-
-//Génère une valeur aléatoire entre 10 et 100 et la remplace dans .GPU
-function mettreAJourGPU(selecteur) {
-    // Sélectionne tous les éléments avec la classe spécifiée (par exemple, ".RAM")
-    var elementsRAM = document.querySelectorAll(selecteur);
-  
-    // Met à jour le texte de chaque élément toutes les 1 seconde
-    setInterval(function() {
-      elementsRAM.forEach(function(element) {
-        // Génère un nombre aléatoire entre 600 et 630
-        var valeurAleatoire = Math.floor(Math.random() * 88) + 10;
-  
-        // Remplace le texte de l'élément par la valeur aléatoire
-        element.textContent = valeurAleatoire;
-      });
-    }, 1000); // 1000 millisecondes équivalent à 1 seconde
-  }
-
-
-
-  // Animation du button de controle
-const bt3 = document.querySelectorAll('.button_normal');
-bt3.forEach(button_normal => {
-    button_normal.addEventListener('mouseenter', () => {
-        button_normal.classList.add('button_normal_hover');
-    });
-
-    button_normal.addEventListener('mouseleave', () => {
-        button_normal.classList.remove('button_normal_hover');
-    });
-});
-
-
-
-  // Animation du div flux temp
-window.onload= anim_flux();
-function anim_flux(){
-    const bf = document.querySelectorAll('#bloc_flux');
-    var num = 0;
-    setInterval(() => {
-        bf.forEach(bloc_flux => {
-                if(num === 0){
-                    bloc_flux.id ='bloc_flux';
-                    num = 1;
-                }
-
-                else{
-                    bloc_flux.id ='bloc_flux_style2';
-                    num = 0;
-                }
-            });
-    }, 500);
-    }
-
-  window.onload= mettreAJourGPU('.utilisation');
-
-//Génère une valeur aléatoire entre 120 et 180 et la remplace dans .GPU
-function mettreAJourGPU(selecteur) {
-    // Sélectionne tous les éléments avec la classe spécifiée (par exemple, ".RAM")
-    var elementsRAM = document.querySelectorAll(selecteur);
-  
-    // Met à jour le texte de chaque élément toutes les 1 seconde
-    setInterval(function() {
-      elementsRAM.forEach(function(element) {
-        // Génère un nombre aléatoire entre 600 et 630
-        var valeurAleatoire = Math.floor(Math.random() * 60) + 120;
-  
-        // Remplace le texte de l'élément par la valeur aléatoire
-        element.textContent = valeurAleatoire;
-      });
-    }, 1000); // 1000 millisecondes équivalent à 1 seconde
-  }
-
-
-//Fonction qui lorsqu'on appuie sur le bouton stopper, change le mode display du div de confirmation 
-function stopperonclick(){
-    const btstop = document.getElementById('validation');
-    btstop.style.display = 'block';
-}
-
-
-//Fonction qui lorsqu'on appuie sur la réponse non, le div de confirmation n'est plus affiché
-function nononclick(){
-    const btnon = document.getElementById('validation');
-    const mdp = document.getElementById('mdp_stop');
-    btnon.style.display = 'none';
-    mdp.style.display = 'none';
-    document.getElementById("errorText").style.display = 'none'
-}
-
-
-//Fonction qui lorsqu'on appuie sur le bouton oui, une zone de texte pour entrer un mod de passe apparaît
-function ouionclick(){
-    const btoui = document.getElementById('mdp_stop');
-    btoui.style.display = 'block';
-}
-
-
-//Action du bouton login
-async function validateStop(){
-    console.log("Fonction validateStop appelée");
-    var password = document.getElementById("confirm_stop").value; //On récupère le mot de passe dans password
-
-    const password_hash = await hashString(password); //On récupère notre password haché
-    console.log( password + " = " + password_hash);
-
-    // Vérifiez d'abord si les champs sont remplis
-    if (password.trim() === ""){
-        // Affichez un message d'erreur si les champs sont vides
-        document.getElementById("errorText").style.display = 'block'
-        document.getElementById("errorText").textContent = "Veuillez remplir le champs.";
-        return; // Arrêtez la fonction si les champs ne sont pas remplis
-    }
-
-    // Exemple : Paires identifiant-mot de passe valides
-    var validCredentials = "360b757d198055e82eb488e31a3544feb316c64cfed3da5e60504c1fd6f5d0aa";
-
-    if (validCredentials === password_hash) {
-        // Authentification réussie, redirigez l'utilisateur vers la page souhaitée
-        console.log("Redirection effectuée");
-        // Attendez 5 secondes avant de masquer l'écran de chargement
-        var loader = document.getElementById('loader');
-        loader.style.display = 'flex';
-        setTimeout(function() {
-            var loader = document.getElementById('loader');
-            loader.style.display = 'none';
-            window.location.href= "https://google.fr";
-        },10000); // temps de chargement en millisecondes aléatoire
-        
-    } else {
-        // Affichez un message d'erreur
-        document.getElementById("errorText").textContent = "Mot de passe incorrect.";
-    }
-}
